82147b685a90ea0e5b2e20904b63d97b3c9c393a
clickout <mick.voros@gmail.com>
2023-06-13 22:35:32 +0200

    Base Test - Remove unnecessary configuration for SchemaIntegrationTestCase

--- base-test/src/main/java/ai/stapi/test/schemaintegration/SchemaIntegrationTestCase.java
+++ base-test/src/main/java/ai/stapi/test/schemaintegration/SchemaIntegrationTestCase.java
@@ -1,9 +1,7 @@
 package ai.stapi.test.schemaintegration;
 
 import org.junit.jupiter.api.Tag;
-import org.springframework.context.annotation.Import;
 
 @Tag("schema-integration")
-@Import(SchemaIntegrationTestConfig.class)
 public class SchemaIntegrationTestCase extends AbstractSchemaIntegrationTestCase {
 }
