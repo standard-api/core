e01dd2e9ef1b6c18dae866ab9859ce55f051aef3
Tomáš Čížek <tomascizek@gmail.com>
2023-07-20 19:04:28 +0200

    Omit extensions in forms by default for now

--- form-api/src/main/java/ai/stapi/formapi/FormEndpoint.java
+++ form-api/src/main/java/ai/stapi/formapi/FormEndpoint.java
@@ -7,6 +7,7 @@ import ai.stapi.graphsystem.operationdefinition.model.OperationDefinitionProvide
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,17 +31,21 @@ public class FormEndpoint {
     this.operationDefinitionProvider = operationDefinitionProvider;
   }
 
-  @GetMapping("/form/{operationId}/")
+  @GetMapping({
+      "/form/{operationId}",
+      "/form/{operationId}/{resourceId}",
+  })
   @ResponseBody
   public Map<String, Object> form(
       @PathVariable String operationId,
-      @PathVariable String resourceId,
-      @PathVariable String startModificationNodeIdAndType
+      @PathVariable(required = false) String resourceId,
+      @RequestParam(required = false) String startModificationNodeIdAndType,
+      @RequestParam(required = false, defaultValue = "true") Boolean omitExtension
   ) {
     var operation = this.operationDefinitionProvider.provide(operationId);
 
     return Map.of(
-        "formSchema", this.jsonSchemaMapper.map(operation),
+        "formSchema", this.jsonSchemaMapper.map(operation, omitExtension),
         "uiSchema", this.uiSchemaLoader.load(operation),
         "formData", this.formDataLoader.load(operation, resourceId, startModificationNodeIdAndType)
     );
